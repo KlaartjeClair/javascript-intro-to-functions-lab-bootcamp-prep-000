181f253031ff88420a10b2f12aac6dc3a0e88619
Klaartje Claire <rosemarieclaire@gmail.com>
2020-08-20 14:38:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ function shout(string)
  return string.toUpperCase();
 }
 function logShout(string)toUpperCase
+<<<<<<< HEAD
  console.log ('');
+=======
+ console.log ('STRING');
+>>>>>>> f05d4ea8102e5676537c63f3be369939f5ff6654
  
 function whisper(string)
 {
   return string.toLowerCase();
 }
+<<<<<<< HEAD
 function logWhisper(string)toLowerCase
 console.log ('');
+=======
+logWhisper('HELLO!'.toLowerCase());
+console.log ('hello')
+>>>>>>> f05d4ea8102e5676537c63f3be369939f5ff6654
